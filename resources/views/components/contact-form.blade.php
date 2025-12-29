e237ecd5441d6cebb66d6f7cdbe3ad8979248829
zuhair <zowhair@gmail.com>
2025-12-29 11:55:29 +0500

    Refactor contact form layout and fields

--- resources/views/components/contact-form.blade.php
+++ resources/views/components/contact-form.blade.php
@@ -1,84 +1,110 @@
 <section class="resume-section p-4 p-lg-5 text-center" id="contact">
-        <div class="my-auto">
-          <h2 class="mb-4"> Stay Connected!! </h2>
+  <div class="my-auto">
 
-          <ul class="fa-ul mb-4 ml-0">
-            <li id="mail-address">
-<!--               Replace with your email address -->
-                <i class="fas fa-envelope-open mr-2 contact-icons"></i
-                ><a href="mailto:info@expeditionasia.com.pk">info@expeditionasia.com.pk</a>
-            </li>
-            <li id="mail-address">
-<!--               Replace with your email address -->
-                <i class="fas fa-envelope-open mr-2 contact-icons"></i
-                ><a href="mailto:expeditionasiaa@gmail.com">expeditionasiaa@gmail.com</a>
-            </li>
-            <li>
-              <i class="fas fa-mobile-alt mr-2 contact-icons"></i><a style="color:#282e6a;" href="tel:+923004708813">+92 (300) 4708813</a>
-            </li>
-            <li>
-              <i class="fas fa-mobile-alt mr-2 contact-icons"></i><a style="color:#282e6a;" href="tel:+925815456667">+92 (58154) 56667</a>
-            </li>
-            <li>
-              <i class="fas fa-map-marker-alt mr-2 contact-icons"></i>College Road, Skardu
-            </li>
-            <li>
-              <i class="fas fa-map-marker-alt mr-2 contact-icons"></i>27-E 3rd Floor office # 5 Ali Plaza Fazl e Haq Road, Blue area, Islamabad Pakistan
-            </li>
-          </ul>
+    <!-- Heading -->
+    <h2 class="mb-4">Stay Connected!!</h2>
 
-          <p>
-            ...or leave your message below and I will be back to you as soon as
-            possible.
-          </p>
+    <!-- Address Section -->
+    <div class="container mb-4">
+      <div class="row text-left align-items-start">
 
-          <form
-            class="contact-form d-flex flex-column align-items-center"
-            action="{{ url('/send') }}"
-            method="POST"
-          >
-          @csrf()
-           {{ csrf_field() }}
-            <div class="form-group w-75">
-              <input
-                type="name"
-                class="form-control"
-                placeholder="Name"
-                name="name"
-                required
-              />
-            </div>
-            <div class="form-group w-75">
-              <input
-                type="email"
-                class="form-control"
-                placeholder="Email"
-                name="name"
-                required
-              />
-            </div>
-            <div class="form-group w-75">
-              <input
-                type="phone"
-                class="form-control"
-                placeholder="phone +12 00 000 0000"
-                name="phone"
-                required
-              />
-            </div>
-
-            <div class="form-group w-75">
-              <textarea
-                class="form-control"
-                type="text"
-                placeholder="Message"
-                rows="7"
-                name="message"
-                required
-              ></textarea>
-            </div>
-
-            <button type="submit" class="btn btn-submit btn-info w-75">Submit</button>
-          </form>
+        <!-- Skardu -->
+        <div class="col-12 col-md-3 font-weight-bold">
+          Skardu
         </div>
-      </section>
+        <div class="col-12 col-md-9 text-muted">
+          College Road, Skardu
+        </div>
+
+        <!-- Islamabad -->
+        <div class="col-12 col-md-3 font-weight-bold mt-3 mt-md-2">
+          Islamabad
+        </div>
+        <div class="col-12 col-md-9 text-muted mt-md-2">
+          27-E, 3rd Floor, Office # 5, Ali Plaza, Fazl-e-Haq Road, Blue Area, Islamabad, Pakistan
+        </div>
+
+      </div>
+    </div>
+
+    <!-- Contact Info -->
+    <ul class="fa-ul mb-4 ml-0 text-left mx-auto" style="max-width: 600px;">
+      <li>
+        <i class="fas fa-envelope-open mr-2 contact-icons"></i>
+        <a href="mailto:info@expeditionasia.com.pk">info@expeditionasia.com.pk</a>
+      </li>
+      <li>
+        <i class="fas fa-envelope-open mr-2 contact-icons"></i>
+        <a href="mailto:expeditionasiaa@gmail.com">expeditionasiaa@gmail.com</a>
+      </li>
+      <li>
+        <i class="fas fa-mobile-alt mr-2 contact-icons"></i>
+        <a style="color:#282e6a;" href="tel:+923004708813">+92 (300) 4708813</a>
+      </li>
+      <li>
+        <i class="fas fa-mobile-alt mr-2 contact-icons"></i>
+        <a style="color:#282e6a;" href="tel:+925815456667">+92 (58154) 56667</a>
+      </li>
+    </ul>
+
+    <!-- Message Text -->
+    <p>
+      ...or leave your message below and I will be back to you as soon as possible.
+    </p>
+
+    <!-- Contact Form -->
+    <form
+      class="contact-form d-flex flex-column align-items-center"
+      action="{{ url('/send') }}"
+      method="POST"
+    >
+      @csrf()
+      {{ csrf_field() }}
+
+      <div class="form-group w-75">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Name"
+          name="name"
+          required
+        />
+      </div>
+
+      <div class="form-group w-75">
+        <input
+          type="email"
+          class="form-control"
+          placeholder="Email"
+          name="email"
+          required
+        />
+      </div>
+
+      <div class="form-group w-75">
+        <input
+          type="tel"
+          class="form-control"
+          placeholder="Phone +12 00 000 0000"
+          name="phone"
+          required
+        />
+      </div>
+
+      <div class="form-group w-75">
+        <textarea
+          class="form-control"
+          placeholder="Message"
+          rows="7"
+          name="message"
+          required
+        ></textarea>
+      </div>
+
+      <button type="submit" class="btn btn-submit btn-info w-75">
+        Submit
+      </button>
+    </form>
+
+  </div>
+</section>
